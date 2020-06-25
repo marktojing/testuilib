248f95823fcbdbc4c4669428c3bc402b5d3af804
jinglei <965334541@qq.com>
2020-06-25 11:46:10 +0800

    增加界面2

---
+++ testlib/src/main/java/com/csnt/testlib/DefaultTheme.java
@@ -0,0 +1,10 @@
+package com.csnt.testlib;
+
+/**
+ * Created by sunrain
+ * Created Date 2020/6/10 3:56 PM
+ */
+public interface DefaultTheme {
+    public abstract void setDefaultTheme();
+    public abstract void setDefaultSmallTheme(float rate);
+}
---
+++ testlib/src/main/java/com/csnt/testlib/viewUtils/ColorUtil.java
@@ -0,0 +1,11 @@
+package com.csnt.testlib.viewUtils;
+
+/**
+ * Created by sunrain
+ * Created Date 2020/6/9 12:02 AM
+ */
+public class ColorUtil {
+    public static int setAlpha(int percent,int color){
+        return ((255*percent/100)<<24)|color;
+    }
+}
