5a341d5416437c3ba7c43dc52aa846bacf8ed8c1
jinglei <965334541@qq.com>
2020-06-25 11:23:57 +0800

    增加界面

---
+++ app/src/main/java/com/csnt/testuilib/MainActivity.java
@@ -0,0 +1,18 @@
+package com.csnt.testuilib;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+/**
+ * Created by sunrain
+ * Created Date 2020/6/25 11:21 AM
+ */
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
