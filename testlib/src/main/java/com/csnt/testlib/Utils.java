5dc97f0994561576e7bf41b699631ae290faa386
jinglei <965334541@qq.com>
2020-06-25 11:09:22 +0800

    Initial commit

---
+++ testlib/src/main/java/com/csnt/testlib/Utils.java
@@ -0,0 +1,13 @@
+package com.csnt.testlib;
+
+import android.text.TextUtils;
+
+/**
+ * Created by sunrain
+ * Created Date 2020/6/25 10:55 AM
+ */
+public class Utils {
+    public static boolean isEmpty(String str){
+        return TextUtils.isEmpty(str);
+    }
+}
